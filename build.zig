a74b90698c317db7529deef3cefa1f0dee0ce86c
timeolord <timeolord6677@gmail.com>
2025-11-28 19:21:09 -0500

    added zig build check --watch

--- build.zig
+++ build.zig
@@ -4,16 +4,25 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const exe_mod = b.addModule("vaster", .{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
     const exe = b.addExecutable(.{
         .name = "vaster",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = exe_mod,
     });
 
     b.installArtifact(exe);
+
+    const exe_check = b.addExecutable(.{
+        .name = "foo",
+        .root_module = exe_mod,
+    });
+
+    const check = b.step("check", "Check if vaster compiles");
+    check.dependOn(&exe_check.step);
 
     const run_step = b.step("run", "Run the app");
 
