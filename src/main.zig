d18c25b7e5bed2e2adec900decb69a0d1bd79db6
anotherLostKitten <eggismagicchickensquared@gmail.com>
2025-11-28 19:13:54 -0500

    remove boilerplate

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
-const learning_zig = @import("learning_zig");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try learning_zig.bufferedPrint();
 }
